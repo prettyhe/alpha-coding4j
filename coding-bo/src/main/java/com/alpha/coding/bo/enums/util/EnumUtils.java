636f7c96d728b1879224d2a990790605788601ae
prettyhe <prettyhe>
2020-10-09 14:48:18 +0800

    update

--- coding-bo/src/main/java/com/alpha/coding/bo/enums/util/EnumUtils.java
+++ coding-bo/src/main/java/com/alpha/coding/bo/enums/util/EnumUtils.java
@@ -3,6 +3,9 @@
  */
 package com.alpha.coding.bo.enums.util;
 
+import java.math.BigDecimal;
+import java.util.Comparator;
+
 /**
  * EnumUtils
  *
@@ -11,9 +14,47 @@ package com.alpha.coding.bo.enums.util;
  */
 public class EnumUtils {
 
+    /**
+     * 比较器，相等返回0，不等返回-1
+     */
+    public static Comparator IGNORE_TYPE_COMPARATOR = (o1, o2) -> {
+        if (o1 == null && o2 == null) {
+            return 0;
+        }
+        if (o1 == null || o2 == null) {
+            return -1;
+        }
+        if (o1.equals(o2)) {
+            return 0;
+        }
+        try {
+            BigDecimal bd1 = new BigDecimal(String.valueOf(o1).trim());
+            BigDecimal bd2 = new BigDecimal(String.valueOf(o2).trim());
+            if (bd1.compareTo(bd2) == 0) {
+                return 0;
+            }
+        } catch (Exception e) {
+            // 非数值
+        }
+        if (String.valueOf(o1).trim().equals(String.valueOf(o2).trim())) {
+            return 0;
+        }
+        return -1;
+    };
+
     public static <T extends Enum<T> & EnumWithCodeSupplier> T parse(Class<T> enumClass, Object code) {
         for (T t : enumClass.getEnumConstants()) {
             if ((code == null && t.codeSupply().get() == null) || (code != null && code.equals(t.codeSupply().get()))) {
+                return t;
+            }
+        }
+        throw new IllegalArgumentException("Unknown code = " + code + " for enum class = " + enumClass);
+    }
+
+    public static <T extends Enum<T> & EnumWithCodeSupplier> T parse(Class<T> enumClass, Object code,
+                                                                     Comparator comparator) {
+        for (T t : enumClass.getEnumConstants()) {
+            if (comparator.compare(code, t.codeSupply().get()) == 0) {
                 return t;
             }
         }
@@ -28,13 +69,13 @@ public class EnumUtils {
         }
     }
 
-    public static <T extends Enum<T>> T safeParseByName(Class<T> enumClass, String name) {
-        for (T t : enumClass.getEnumConstants()) {
-            if (t.name().equals(name)) {
-                return t;
-            }
+    public static <T extends Enum<T> & EnumWithCodeSupplier> T safeParse(Class<T> enumClass, Object code,
+                                                                         Comparator comparator) {
+        try {
+            return parse(enumClass, code, comparator);
+        } catch (IllegalArgumentException e) {
+            return null;
         }
-        return null;
     }
 
     public static <T> T safeParseEnumByName(Class<T> enumClass, String name) {
